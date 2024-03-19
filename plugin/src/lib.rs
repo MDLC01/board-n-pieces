873280947bbe718a067df602411f1f17efa25004
MDLC01 <57839069+MDLC01@users.noreply.github.com>
2024-03-19 19:57:41 +0100

    Improve package loading time
    - Do not load the plugin on package load.
    - Only load default piece images when the `display-board` function is called, instead of at function declaration.

--- plugin/src/lib.rs
+++ plugin/src/lib.rs
@@ -4,15 +4,9 @@ mod model;
 mod utils;
 
 use crate::abi::Abi;
-use crate::model::Position;
 use wasm_minimal_protocol::{initiate_protocol, wasm_func};
 
 initiate_protocol!();
-
-#[wasm_func]
-pub fn starting_position() -> Vec<u8> {
-    Position::default().to_vec()
-}
 
 #[wasm_func]
 pub fn parse_fen(fen: &[u8]) -> abi::Result<Vec<u8>> {
