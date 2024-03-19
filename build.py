b1a0315d275a92aa053009fad030060ccf2624bc
MDLC01 <57839069+MDLC01@users.noreply.github.com>
2024-03-19 14:40:51 +0100

    Fix build script

--- build.py
+++ build.py
@@ -9,17 +9,60 @@ from pathlib import Path
 
 
 LICENSE = Path('LICENSE')
-TARGET_DIRECTORY = Path('target/')
+TARGET_DIR = Path('target/')
 
 
-def preprocess_readme(content: str) -> str:
-    EXAMPLES_DIRECTORY = TARGET_DIRECTORY.joinpath('examples')
+def read_exclude_file(path: Path) -> list[str]:
+    with open(path) as f:
+        lines = [line.strip() for line in f.read().splitlines()]
+    return [os.path.normcase(entry) for entry in lines if entry and not entry.startswith('#')]
+
+
+def clone_sources():
+    LIBRARY_DIR = Path('src/')
+    EXCLUDE_FILE = LIBRARY_DIR.joinpath('.exclude')
+
+    excluded_files = read_exclude_file(EXCLUDE_FILE)
+    excluded_files.append(os.path.normcase(EXCLUDE_FILE.relative_to(LIBRARY_DIR)))
+
+    for path, directories, files in os.walk(LIBRARY_DIR):
+        for file in files:
+            file_name = Path(path).relative_to(LIBRARY_DIR).joinpath(file)
+            if os.path.normcase(file_name) not in excluded_files:
+                source = LIBRARY_DIR.joinpath(file_name)
+                destination = TARGET_DIR.joinpath(file_name)
+                destination.parent.mkdir(parents=True, exist_ok=True)
+                shutil.copyfile(source, destination)
+    shutil.copyfile(LICENSE, TARGET_DIR.joinpath('LICENSE'))
+
+
+def build_plugin():
+    BUILD_TARGET = 'wasm32-unknown-unknown'
+    PLUGIN_DIR = Path('plugin/')
+    PLUGIN_PATH = PLUGIN_DIR.joinpath('target', BUILD_TARGET, 'release', 'board_n_pieces_plugin.wasm')
+    PLUGIN_NAME = 'plugin.wasm'
+
+    subprocess.run(
+        ['cargo', 'build', '--release', '--target', BUILD_TARGET, '--quiet'],
+        cwd=PLUGIN_DIR,
+        check=True,
+    )
+
+    shutil.copyfile(PLUGIN_PATH, TARGET_DIR.joinpath(PLUGIN_NAME))
+
+
+def build_readme():
+    README = Path('README.md')
+    EXAMPLES_DIR = Path('examples')
+
+    with open(TARGET_DIR.joinpath(README)) as f:
+        readme = f.read()
 
     examples = []
     new_lines = []
     example = []
     is_example = False
-    for line in content.splitlines():
+    for line in readme.splitlines():
         new_lines.append(line)
         if is_example and line.startswith('```'):
             is_example = False
@@ -34,77 +77,33 @@ def preprocess_readme(content: str) -> str:
             example = []
 
     example_source = [
-        '#import "src/lib.typ": *;',
+        f'#import "lib.typ": *;',
         '#set page(width: auto, height: auto, margin: 0cm);',
     ]
     for example in examples:
         example_source.append(f'#page[{example}];')
 
-    EXAMPLES_DIRECTORY.mkdir(parents=True)
+    TARGET_DIR.joinpath(EXAMPLES_DIR).mkdir(parents=True)
     subprocess.run(
-        ['typst', 'compile', '-', str(EXAMPLES_DIRECTORY.joinpath('example-{n}.png'))],
+        ['typst', 'compile', '-', str(EXAMPLES_DIR.joinpath('example-{n}.png'))],
         input='\n'.join(example_source),
         encoding='utf-8',
+        cwd=TARGET_DIR,
         check=True,
     )
 
-    return '\n'.join(new_lines)
-
-
-def read_exclude_file(path: Path) -> list[str]:
-    with open(path) as f:
-        lines = [line.strip() for line in f.read().splitlines()]
-    return [os.path.normcase(entry) for entry in lines if entry and not entry.startswith('#')]
-
-
-def clone_sources():
-    LIBRARY_DIRECTORY = Path('src/')
-    EXCLUDE_FILE = LIBRARY_DIRECTORY.joinpath('.exclude')
-    README = Path('README.md')
-
-    if TARGET_DIRECTORY.exists():
-        shutil.rmtree(TARGET_DIRECTORY)
-    TARGET_DIRECTORY.mkdir(parents=True)
-
-    excluded_files = read_exclude_file(EXCLUDE_FILE)
-    excluded_files.append(os.path.normcase(EXCLUDE_FILE.relative_to(LIBRARY_DIRECTORY)))
-
-    for path, directories, files in os.walk(LIBRARY_DIRECTORY):
-        for file in files:
-            file_name = Path(path).relative_to(LIBRARY_DIRECTORY).joinpath(file)
-            if os.path.normcase(file_name) not in excluded_files:
-                source = LIBRARY_DIRECTORY.joinpath(file_name)
-                destination = TARGET_DIRECTORY.joinpath(file_name)
-                destination.parent.mkdir(parents=True, exist_ok=True)
-                if os.path.normcase(file_name) == os.path.normcase(README):
-                    with open(source) as f:
-                        readme = f.read()
-                    processed_readme = preprocess_readme(readme)
-                    with open(destination, 'w') as f:
-                        f.write(processed_readme)
-                else:
-                    shutil.copyfile(source, destination)
-    shutil.copyfile(LICENSE, TARGET_DIRECTORY.joinpath('LICENSE'))
-
-
-def build_plugin():
-    BUILD_TARGET = 'wasm32-unknown-unknown'
-    PLUGIN_DIRECTORY = Path('plugin/')
-    PLUGIN_PATH = PLUGIN_DIRECTORY.joinpath('target', BUILD_TARGET, 'release', 'board_n_pieces_plugin.wasm')
-    PLUGIN_NAME = 'plugin.wasm'
-
-    subprocess.run(
-        ['cargo', 'build', '--release', '--target', BUILD_TARGET, '--quiet'],
-        cwd=PLUGIN_DIRECTORY,
-        check=True,
-    )
-
-    shutil.copyfile(PLUGIN_PATH, TARGET_DIRECTORY.joinpath(PLUGIN_NAME))
+    with open(TARGET_DIR.joinpath(README), 'w') as f:
+        f.write('\n'.join(new_lines))
 
 
 def main():
+    if TARGET_DIR.exists():
+        shutil.rmtree(TARGET_DIR)
+    TARGET_DIR.mkdir(parents=True)
+
     clone_sources()
     build_plugin()
+    build_readme()
 
 
 if __name__ == '__main__':
